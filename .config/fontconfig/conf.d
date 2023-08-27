a3401159c2f3635987ed226b9d6777c03bb7ba9d
lvisb <luis.flashdev@gmail.com>
2023-08-27 11:17:03 -0300

    feat(fontconfig): conf.d

---
+++ .config/fontconfig/conf.d
@@ -0,0 +1,40 @@
+<?xml version="1.0"?>
+<!DOCTYPE fontconfig SYSTEM "urn:fontconfig:fonts.dtd">
+<fontconfig>
+
+  <alias>
+   <family>Hack Nerd Font</family>
+   <prefer>
+    <family>Hack Nerd Font</family>
+    <family>Font Awesome 6 Pro</family>
+    <family>Font Awesome 6 Brands</family>
+    <family>Font Awesome 6 Duotone</family>
+   </prefer>
+  </alias>
+
+  <match target="font">
+    <edit name="antialias" mode="assign">
+      <bool>true</bool>
+    </edit>
+  </match>
+
+  <match target="font">
+    <edit name="hinting" mode="assign">
+      <bool>true</bool>
+    </edit>
+  </match>
+
+  <match target="font">
+    <edit name="hintstyle" mode="assign">
+      <const>hintfull</const>
+    </edit>
+  </match>
+
+  <match target="font">
+    <edit name="rgba" mode="assign">
+      <const>rgb</const>
+    </edit>
+  </match>
+
+</fontconfig>
+
