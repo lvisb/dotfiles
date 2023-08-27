af22f92ffb15df36db5c222b927ccbaa5fbea466
lvisb <luis.flashdev@gmail.com>
2023-08-26 22:17:32 -0300

    feat(eww): battery color

--- .config/eww/widgets/battery/battery.sh
+++ .config/eww/widgets/battery/battery.sh
@@ -3,10 +3,27 @@
 # upower -i /org/freedesktop/UPower/devices/battery_BAT0
 loaded=false
 
-battery_icon() {
+battery_info() {
   local info=$(upower -i /org/freedesktop/UPower/devices/battery_BAT0)
-  local percent=$(echo "$info" | grep percentage | grep -oE '[0-9]+' | head -n1)
-  local state=$(echo "$info" | grep state | awk '{print $2}')
+
+  echo "$info"
+}
+
+battery_percent() {
+  local percent=$(echo "$(battery_info)" | grep percentage | grep -oE '[0-9]+' | head -n1)
+
+  echo "$percent"
+}
+
+battery_cycles() {
+  local cycles=$(echo "$(battery_info)" | grep "charge-cycles" | grep -oE '[0-9]+' | head -n1)
+
+  echo "$cycles"
+}
+
+battery_icon() {
+  local percent=$(battery_percent)
+  local state=$(echo "$(battery_info)" | grep state | awk '{print $2}')
   local is_charging=0
 
   if [ $state == "charging" ]; then
@@ -15,7 +32,7 @@ battery_icon() {
     is_charging=0
   fi
 
-  if [ $percent -ge 99 ]; then
+  if [ $percent -ge 99 ] || [ $state == "fully-charged" ] ; then
     if [ $is_charging == 1 ]; then
       echo "󰂅"
     else
@@ -85,8 +102,12 @@ battery_icon() {
 }
 
 json() {
+  local cycles=$(printf "%'d" $(battery_cycles))
+
   json="{"
-  json+="\"battery_icon\":\"$(battery_icon)\""
+  json+="\"battery_icon\":\"$(battery_icon)\","
+  json+="\"battery_percent\":\"$(battery_percent)\","
+  json+="\"battery_cycles\":\"${cycles//,/.}\""
   json+="}"
 
   echo "$json"
