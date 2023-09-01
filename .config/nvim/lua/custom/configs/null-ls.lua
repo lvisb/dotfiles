d3ad894ab597bd4631f328889b9b2a7e7c4eeb57
lvisb <luis.flashdev@gmail.com>
2023-09-01 16:34:22 -0300

    feat(nvim): go lsp

--- .config/nvim/lua/custom/configs/null-ls.lua
+++ .config/nvim/lua/custom/configs/null-ls.lua
@@ -5,7 +5,7 @@ local b = null_ls.builtins
 local sources = {
 
   -- webdev stuff
-  b.formatting.prettier.with {  }, 
+  b.formatting.prettier.with {},
   b.formatting.eslint_d.with { filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact" } },
 
   -- Lua
@@ -13,6 +13,19 @@ local sources = {
 
   -- cpp
   b.formatting.clang_format,
+
+  -- go
+  b.formatting.goimports_reviser,
+  b.formatting.gofumpt,
+  b.diagnostics.golangci_lint.with {
+    filetypes = { "go" },
+    args = {
+      "run",
+      "--fix=false",
+      "--out-format=json",
+      "-E", "revive"
+    },
+  },
 }
 
 null_ls.setup {
