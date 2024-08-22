e6895f6c29c78ee8a9c30041ff4efab5af8b8c1e
lvisb <luis.flashdev@gmail.com>
2024-08-22 18:41:03 -0300

    fix(nvchad): term config

--- .config/nvim/lua/chadrc.lua
+++ .config/nvim/lua/chadrc.lua
@@ -13,15 +13,28 @@ M.ui = {
   hl_override = highlights,
   hl_add = highlights.add,
 
-  term = {
-    float = {
-      relative = "editor",
-      row = 0.17,
-      col = 0.16,
-      width = 0.7,
-      height = 0.6,
-      border = "single",
-    },
+  -- term = {
+  --   float = {
+  --     relative = "editor",
+  --     row = 0.17,
+  --     col = 0.16,
+  --     width = 0.7,
+  --     height = 0.6,
+  --     border = "single",
+  --   },
+  -- },
+}
+
+M.term = {
+  winopts = { number = false, relativenumber = false },
+  sizes = { sp = 0.3, vsp = 0.2, ["bo sp"] = 0.3, ["bo vsp"] = 0.2 },
+  float = {
+    relative = "editor",
+    row = 0.17,
+    col = 0.16,
+    width = 0.7,
+    height = 0.6,
+    border = "single",
   },
 }
 
