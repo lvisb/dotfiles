a93f1d5fd63db8282bf4e279e7b3c6cd68401e93
lvisb <luis.flashdev@gmail.com>
2024-10-01 16:26:24 -0300

    fix(systemd): backup was not being completed

--- .config/systemd/user/backup.sh
+++ .config/systemd/user/backup.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
 wowDir="$HOME/Games/battlenet/drive_c/Program Files (x86)/World of Warcraft/_retail_"
-
 destinationDir="$HOME/gdrive/archlinux-backup"
 wowDestinationDir="$destinationDir/wow"
 homeDestinationDir="$destinationDir/home"
 
 mkdir -p "$destinationDir"
 
-nohup rclone copy "$wowDir/Interface/AddOns" "$wowDestinationDir/Interface/AddOns" > /tmp/rclone.log 2>&1 &
-nohup rclone copy "$wowDir/WTF" "$wowDestinationDir/WTF" > /tmp/rclone.log 2>&1 &
-nohup rclone copy "$HOME/.ssh" "$homeDestinationDir/.ssh" > /tmp/rclone.log 2>&1 &
-nohup rclone copy "$HOME/.config/rclone" "$homeDestinationDir/.config/rclone" > /tmp/rclone.log 2>&1 &
-nohup rclone copy "$HOME/.config/datagrip" "$homeDestinationDir/.config/datagrip" > /tmp/rclone.log 2>&1 &
-nohup rclone copy "$HOME/.zsh_history" "$homeDestinationDir/.zsh_history" > /tmp/rclone.log 2>&1 &
-nohup rclone copy "$HOME/.local/share/fonts" "$homeDestinationDir/.local/share/fonts" > /tmp/rclone.log 2>&1 &
-nohup rclone copy "$HOME/.gitconfig" "$homeDestinationDir/" > /tmp/rclone.log 2>&1 &
-nohup rclone copy "$HOME/developer/books" "$homeDestinationDir/developer/books" > /tmp/rclone.log 2>&1 &
+# Copiando arquivos do WoW
+nohup rclone copy "$wowDir/Interface/AddOns" "$wowDestinationDir/Interface/AddOns" > /tmp/rclone-wow-addons.log 2>&1 &
+nohup rclone copy "$wowDir/WTF" "$wowDestinationDir/WTF" > /tmp/rclone-wow-wtf.log 2>&1 &
 
+# Copiando arquivos da home
+nohup rclone copy "$HOME/.ssh" "$homeDestinationDir/.ssh" > /tmp/rclone-ssh.log 2>&1 &
+nohup rclone copy "$HOME/.config/rclone" "$homeDestinationDir/.config/rclone" > /tmp/rclone-config-rclone.log 2>&1 &
+nohup rclone copy "$HOME/.config/datagrip" "$homeDestinationDir/.config/datagrip" > /tmp/rclone-config-datagrip.log 2>&1 &
+nohup rclone copy "$HOME/.zsh_history" "$homeDestinationDir/.zsh_history" > /tmp/rclone-zsh-history.log 2>&1 &
+nohup rclone copy "$HOME/.local/share/fonts" "$homeDestinationDir/.local/share/fonts" > /tmp/rclone-fonts.log 2>&1 &
+nohup rclone copy "$HOME/.gitconfig" "$homeDestinationDir/" > /tmp/rclone-gitconfig.log 2>&1 &
+nohup rclone copy "$HOME/developer/books" "$homeDestinationDir/developer/books" > /tmp/rclone-books.log 2>&1 &
+
+# Salvando lista de apps do flatpak e pacotes AUR
 flatpak list --app --columns=application | awk '{print "flatpak install " $1}' > "$destinationDir/flatpak-apps.txt"
-
 paru -Qe | awk '{print $1}' > "$destinationDir/aur-packages.txt"
-
-echo "$(date '+%Y-%m-%d %H:%M:%S')" >> "$destinationDir/backup-history.txt"
